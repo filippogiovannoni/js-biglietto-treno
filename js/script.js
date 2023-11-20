5b9f5b9983ab58b5c961608d96ed0f2de5b04501
filippogiovannoni <filippogiovannonii@gmail.com>
2023-11-20 15:52:25 +0100

    add conditional rules and two decimal places

--- js/script.js
+++ js/script.js
@@ -14,13 +14,34 @@ Tariffario
 
 
 // Chiedere all'utente quanti km vuole percorrere
-const km_percorsi = prompt ('Quanti chilometri vuoi percorrere per questa tratta?')
-
+const km_percorsi = parseInt(prompt ('Quanti chilometri vuoi percorrere per questa tratta?'));
 
 // Chiedere all'utente quanti anni ha
-const user_age = prompt ('Quanti anni hai?')
+const user_age = parseInt(prompt ('Quanti anni hai?'));
 
+// Tariffario per chilometri percorsi
+const km_price = (0.21 * km_percorsi);
 
+// Se il passeggero ha un'età minore di 18 anni, verrà applicato uno sconto del 20%
+if (user_age < 18) {
+    
+    const under_18 = (km_price - (km_price / 100 * 20));
+   
+    // Stampo in pagina il prezzo della tratta per il passeggero minorenne
+    document.getElementById('price').innerHTML = `Il prezzo per la seguente tratta è di: ${under_18.toFixed(2)}`+ "€";
 
-const km_price = (0.21 * km_percorsi)
-console.log (km_price)
+// Altrimenti se il passeggero ha un'età superiore ai 65 anni, verrà applicato uno sconto del 40%
+} else if (user_age > 65) { 
+    
+    const over_65 = (km_price - (km_price / 100 * 40));
+  
+    // Stampo in pagina il prezzo della tratta per il passeggero over 65
+    document.getElementById('price').innerHTML = `Il prezzo per la seguente tratta è di: ${over_65.toFixed(2)}`+ "€";
+
+// Altrimenti il passeggero non potrà usufruire di nessuna scontistica.
+} else {
+
+    // Stampo in pagina il prezzo della tratta per il passeggero.
+    document.getElementById('price').innerHTML = `Il prezzo per la seguente tratta è di: ${km_price.toFixed(2)}`+ "€";
+
+}
